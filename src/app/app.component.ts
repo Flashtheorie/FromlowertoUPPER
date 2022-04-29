fcc7e9b971f19d233e78505ee72a1a31537dd8db
Antoine Pascual <antoinepascualmail@gmail.com>
2022-04-29 18:28:38 +0200

    Check if input rempli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,42 @@ export class AppComponent {
   input: any | undefined
   output: any | undefined
   myNameElem: any
+  inputRempli : any
+  outputRempli : any
+ checkIfInputRempli(){
+   if (this.input !== '')
+   {
+    this.inputRempli = true;
+   }
+   else
+   {
+    this.inputRempli = false;
+   }
+ }
 
+ checkIfOutputRempli(){
+  if (this.output !== '')
+  {
+   this.outputRempli = true;
+  }
+  else
+  {
+   this.outputRempli = false;
+  }
+}
   lowertoupper(){
     this.output = this.input.toUpperCase()
     this.input = '';
+    this.inputRempli = false;
+    this.outputRempli = true;
   }
+
 
   uppertolower(){
     this.input = this.output.toLowerCase();
     this.output = '';
+    this.outputRempli = false;
+    this.inputRempli = true;
   }
   
   
