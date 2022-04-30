410bfbe31219ce7b0798122672cbee39ca049bf1
Antoine Pascual <antoinepascualmail@gmail.com>
2022-04-30 17:33:18 +0200

    Modif homecomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,46 +6,22 @@ import { initializeApp } from "firebase/app";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'From lower to UPPER';
+  title = 'from lower to UPPER';
   input: any | undefined
   output: any | undefined
-  myNameElem: any
-  inputRempli : any
-  outputRempli : any
- checkIfInputRempli(){
-   if (this.input !== '')
-   {
-    this.inputRempli = true;
-   }
-   else
-   {
-    this.inputRempli = false;
-   }
- }
+  
 
- checkIfOutputRempli(){
-  if (this.output !== '')
-  {
-   this.outputRempli = true;
-  }
-  else
-  {
-   this.outputRempli = false;
-  }
-}
   lowertoupper(){
     this.output = this.input.toUpperCase()
     //this.input = '';
-    this.inputRempli = false;
-    this.outputRempli = true;
+
   }
 
 
   uppertolower(){
     this.input = this.output.toLowerCase();
     //this.output = '';
-    this.outputRempli = false;
-    this.inputRempli = true;
+   
   }
   
   
